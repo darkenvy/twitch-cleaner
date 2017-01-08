6bd900c7781f6459300f94cf032b71c63737637d
darkenvy <darkenvy6@gmail.com>
2017-01-08 15:02:33 -0800

    rewrote into object oriented programming

--- twitch-clean.js
+++ twitch-clean.js
@@ -14,124 +14,150 @@
 
 // ------------ //
 
-function spamRating(message) {
-  var isMostlyCaps = function() {
-    var amountOfCaps = 0;
-    for (var i=0; i<message.length; i++) {
-      if (/[A-Z!]/.exec(message[i])) amountOfCaps += 1;
+// function spamRating(message) {
+//   var isMostlyCaps = function() {
+//     var amountOfCaps = 0;
+//     for (var i=0; i<message.length; i++) {
+//       if (/[A-Z!]/.exec(message[i])) amountOfCaps += 1;
+//     }
+//     return (amountOfCaps / message.length > 0.5) ? 3 : 0;
+//   }
+
+//   // Duplicate Words is bugged because the object that is created has 
+//   // keys you dont want to itterate through
+//   // var duplicateWords = function() {
+//   //   var sentance = message.match(/\w+/g);
+//   //   var words = {}
+
+//   //   for (var w in sentance) {
+//   //     if (sentance[w] in words) { words[sentance[w]] += 1 } 
+//   //     else { words[sentance[w]] = 1 }
+//   //   }
+
+//   //   var mostUsed = 0;
+//   //   for (var key in words) {
+//   //     console.log('wk', key, words, words[key]);
+//   //     if (words[key] > mostUsed) mostUsed = words[key]
+//   //   }
+//   //   console.log('mu', mostUsed, sentance.length);
+//   //   return (mostUsed / sentance.length > 0.5) ? 5 : 0;
+//   // }
+
+//   var score = 0;
+//   if (message[0] === "!") score = 7;
+//   if (message[0] === "@") score = 3;
+//   if (message.length < 5) score += 1;
+//   if (message.length > 100) score += parseInt(message.length / 100);
+//   if (message.match(/\w+/g).length === 1) score += 1;
+//   if (/http/.test(message)) score += 3;
+//   score += isMostlyCaps();
+//   // score += duplicateWords();
+
+//   return score;
+// }
+
+// function getColor(currColor, spamRating) {
+//   var rgbColor = currColor.match(/(\d+),\s?(\d+),\s?(\d+)/);
+//   var weight = spamRating >= 10 ? 0.1 : (10-spamRating)/10;
+//   return [
+//       parseInt( parseInt(rgbColor[1]) * weight ),
+//       parseInt( parseInt(rgbColor[2]) * weight ),
+//       parseInt( parseInt(rgbColor[3]) * weight )
+//     ];
+// }
+
+
+
+function Message(idx) {
+  this.text = $('.chat-lines')[0].children[idx].children[5].innerText;
+  this.rating = spamRating(this.text);
+  this.nameColor = $('.chat-lines')[0].children[idx].children[3].style.color;
+  this.textColor = $('.chat-lines')[0].children[idx].children[5].style.color;
+}
+
+Message.prototype= {
+  constructor: Message,
+
+  spamRating: function(message) {
+    var isMostlyCaps = function() {
+      var amountOfCaps = 0;
+      for (var i=0; i<message.length; i++) {
+        if (/[A-Z!]/.exec(message[i])) amountOfCaps += 1;
+      }
+      return (amountOfCaps / message.length > 0.5) ? 3 : 0;
     }
-    return (amountOfCaps / message.length > 0.5) ? 3 : 0;
+
+    // Duplicate Words is bugged because the object that is created has 
+    // keys you dont want to itterate through
+    // var duplicateWords = function() {
+    //   var sentance = message.match(/\w+/g);
+    //   var words = {}
+
+    //   for (var w in sentance) {
+    //     if (sentance[w] in words) { words[sentance[w]] += 1 } 
+    //     else { words[sentance[w]] = 1 }
+    //   }
+
+    //   var mostUsed = 0;
+    //   for (var key in words) {
+    //     console.log('wk', key, words, words[key]);
+    //     if (words[key] > mostUsed) mostUsed = words[key]
+    //   }
+    //   console.log('mu', mostUsed, sentance.length);
+    //   return (mostUsed / sentance.length > 0.5) ? 5 : 0;
+    // }
+
+    var score = 0;
+    if (message[0] === "!") score = 7;
+    if (message[0] === "@") score = 3;
+    if (message.length < 5) score += 1;
+    if (message.length > 100) score += parseInt(message.length / 100);
+    if (message.match(/\w+/g).length === 1) score += 1;
+    if (/http/.test(message)) score += 3;
+    score += isMostlyCaps();
+    // score += duplicateWords();
+
+    return score;
+  },
+
+  generateColor: function(currentColor, spamRating) {
+    var rgbColor = currentColor.match(/(\d+),\s?(\d+),\s?(\d+)/);
+    var weight = spamRating >= 10 ? 0.1 : (10-spamRating)/10;
+    return [
+        parseInt( parseInt(rgbColor[1]) * weight ),
+        parseInt( parseInt(rgbColor[2]) * weight ),
+        parseInt( parseInt(rgbColor[3]) * weight )
+      ];
+  },
+
+  setColor: function(color) {
+    var newNameColor = generateColor(this.nameColor, this.rating)
+    var newTextColor = generateColor(this.textColor, this.rating)
+    // set name color
+    $('.chat-lines')[0].children[idx].children[3].style = "color: rgb(" + newNameColor.toString() + ")";
+    // set text color
+    $('.chat-lines')[0].children[idx].children[5].style = "color: rgb(" + newTextColor.toString() + ")";
   }
-
-  // Duplicate Words is bugged because the object that is created has 
-  // keys you dont want to itterate through
-  // var duplicateWords = function() {
-  //   var sentance = message.match(/\w+/g);
-  //   var words = {}
-
-  //   for (var w in sentance) {
-  //     if (sentance[w] in words) { words[sentance[w]] += 1 } 
-  //     else { words[sentance[w]] = 1 }
-  //   }
-
-  //   var mostUsed = 0;
-  //   for (var key in words) {
-  //     console.log('wk', key, words, words[key]);
-  //     if (words[key] > mostUsed) mostUsed = words[key]
-  //   }
-  //   console.log('mu', mostUsed, sentance.length);
-  //   return (mostUsed / sentance.length > 0.5) ? 5 : 0;
-  // }
-
-  var score = 0;
-  if (message[0] === "!") score = 7;
-  if (message[0] === "@") score = 3;
-  if (message.length < 5) score += 1;
-  if (message.length > 100) score += parseInt(message.length / 100);
-  if (message.match(/\w+/g).length === 1) score += 1;
-  if (/http/.test(message)) score += 3;
-  score += isMostlyCaps();
-  // score += duplicateWords();
-
-  return score;
 }
 
-var spamColor = function(number) {
-  var color;
-  switch(number) {
-    case 0:
-      color = "#BBB";
-      break;
-    case 1:
-      color = "#AAA";
-      break;
-    case 2:
-      color = "#999";
-      break;
-    case 3:
-      color = "#888";
-      break;
-    case 4:
-      color = "#777";
-      break;
-    case 5:
-      color = "#666";
-      break;
-    case 6:
-      color = "#555";
-      break;
-    case 7:
-      color = "#444";
-      break;
-    case 8:
-      color = "#333";
-      break;
-    default:
-      color = "#222";
-      break;
-  }
-  return color;
-}
-
-var lastMessageInt = $('.chat-lines')[0].children.length-1;
-var lastMessage = $('.chat-lines')[0]
-                    .children[lastMessageInt]
-                    .children[5]
-                    .innerText;
-var lastMessageRating = spamRating(lastMessage);
-$('.chat-lines')[0].children[lastMessageInt].children[3]
-                   .style = "color: " + spamColor(lastMessageRating) - 1;
-$('.chat-lines')[0].children[lastMessageInt].children[5]
-                   .style = "color: " + spamColor(lastMessageRating);
-console.log('rating: ', lastMessageRating);
+var latestIdx = $('.chat-lines')[0].children.length-1;
+var lastMessage = new Message(latestIdx);
+console.log(lastMessage.rating, lastMessage.text);
 
 
+// var lastMessageInt = $('.chat-lines')[0].children.length-1;
+// var lastMessage = $('.chat-lines')[0]
+//                     .children[lastMessageInt]
+//                     .children[5]
+//                     .innerText;
+// var lastMessageRating = spamRating(lastMessage);
+// var currColor = 0;
+// $('.chat-lines')[0].children[lastMessageInt].children[3]
+//                    .style = "color: " + getColor(currColor, lastMessageRating) - 1;
+// $('.chat-lines')[0].children[lastMessageInt].children[5]
+//                    .style = "color: " + getColor(currColor, lastMessageRating);
+// console.log('rating: ', lastMessageRating);
 
-
-// console.log("---- Not Spam ----");
-
-// console.log(spamRating("Can you give me a shout out my YouTube is jordan lookabill subscribe I'm new to youtube"));
-// console.log(spamRating("You know Carlsegan42"));
-// console.log(spamRating("thx bro"));
-// console.log(spamRating("Well had an idea"));
-// console.log(spamRating("an i saw pope francis in mexico city, he goes on february on last year"));
-// // console.log(spamRating("btw I actually beat the blind Kaizo! took me an hour and a half though haha"));
-
-// console.log("---- Ignore Special Cases ----");
-
-// console.log(spamRating("@ther8pist Raptor was fun until you learnt to cheese the raptors with the bombs"));
-// console.log(spamRating("2"));
-// console.log(spamRating("Moo 🐮"));
-// console.log(spamRating("@mainman hey hows it going?"));
-// console.log(spamRating("!add movie"));
-
-// console.log("---- Spam ----");
-
-// console.log(spamRating("CoolStoryBob  CoolStoryBob  CoolStoryBob  CoolStoryBob"));
-// console.log(spamRating("I AM YELLING!!!!!!!!!!!!!!"));
-// console.log(spamRating("but now I am not http://renomckenzie.com/"));
-// console.log(spamRating("Can you give me a shout out my YouTube is jordan lookabill subscribe I'm new to youtube Can you give me a shout out my YouTube is jordan lookabill subscribe I'm new to youtube Can you give me a shout out my YouTube is jordan lookabill subscribe I'm new to youtube"));
-// console.log(spamRating("https://discord.gg/carlsagan42 https://discord.gg/carlsagan42 https://discord.gg/carlsagan42 https://discord.gg/carlsagan42"));
 
 
 
